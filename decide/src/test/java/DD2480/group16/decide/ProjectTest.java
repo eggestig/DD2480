ba0669658b845332071b19c2911a04526996bbdc
Anton Sederlin <anton.sederlin@gmail.com>
2024-01-29 18:58:55 +0100

    refrac/issue#14
    Built project with maven

---
+++ decide/src/test/java/DD2480/group16/decide/ProjectTest.java
@@ -0,0 +1,20 @@
+package DD2480.group16.decide;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ProjectTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
