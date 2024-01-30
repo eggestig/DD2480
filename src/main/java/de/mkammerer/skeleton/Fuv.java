fbe1a109a778a71f2e453191c764a0bbbfb34679
iamrobzy <robertinshanghai@me.com>
2024-01-30 08:32:13 +0100

    feature\#5\n\nComputes FUV with randomly generated PUM and PUV

--- src/main/java/de/mkammerer/skeleton/Fuv.java
+++ src/main/java/de/mkammerer/skeleton/Fuv.java
@@ -3,14 +3,56 @@ import java.util.Random;
 
 public class Fuv {
     public static void main(String[] args) throws Exception {
+
+        int i;
+        int j;
+
+        // Generate random PUM - preliminary unlocking matrix
         Random rand = new Random();
-        int len = 50;
-        int[][] data = new int[2][len];
-        for (int i = 0; i < 5; i++) {
-            data[0][i] = rand.nextInt(100);
-            data[1][i] = rand.nextInt(100);
+        int len = 15;
+        int[][] pum = new int[len][len];
+        for (i = 0; i < len; i++) {
+            for  (j = 0; j < len; j++) {
+                pum[i][j] = rand.nextInt(2);
+            }
         }
-          
-        System.out.println("Hello, World!");
+
+        // Generate PUV - preliminary unlocking vector
+        int[] puv = new int[len];
+        for (i = 0; i < len; i++) {
+            puv[i] = rand.nextInt(1);
+        }
+
+        // Extract FUV - final unlocking vector
+        // System.out.println("FUV: ");
+        int[] fuv = new int[len];
+        for (i = 0; i < len; i++) {
+
+            // Condition 1
+            if (puv[i]==0) {
+                fuv[i] = 1;
+                //System.out.print(fuv[i] + (" "));
+                continue;
+            } 
+
+            // Condition 2
+
+            int unlock;
+            for (i = 0; i < len; i++) {
+                unlock = 0;
+
+                for (j = 0; j < len; j++) {
+                    if (pum[i][j]==1) {
+                        unlock = unlock + 1;
+                    }
+                }
+                
+                if (unlock==15) {
+                    fuv[i] = 1;
+                }
+
+               // System.out.print(fuv[i] + (" "));
+            }  
+        }
     }
 }
