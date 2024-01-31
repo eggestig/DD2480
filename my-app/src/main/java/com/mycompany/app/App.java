6f68db2384d12956f42c74275d59ced092ec7c74
Anton Sederlin <anton.sederlin@gmail.com>
2024-01-31 15:59:28 +0100

    fix/#2 Added global vars and parsed main params
    Not testes globaly yet.

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -2,7 +2,55 @@ package com.mycompany.app;
 import java.util.Random;
 
 public class App {
+    static Parameters param;
+    static Boolean[] cmv = new Boolean[15];
+    static int NUMPOINTS;
+    static int[] x_pts;
+    static int[] y_pts;
+    
     public static void main(String[] args) throws Exception {
+
+        Random rand = new Random();
+
+        NUMPOINTS = rand.nextInt(100 - 2) + 2;
+        int p_pts = rand.nextInt(NUMPOINTS - 2) + 2;
+        x_pts = new int[NUMPOINTS];
+        y_pts = new int[NUMPOINTS];
+        param = new Parameters
+                            (
+                            100.0,
+                            10.0,
+                            (double) rand.nextInt(3),
+                            1000.0,
+                            rand.nextInt(NUMPOINTS), 
+                            rand.nextInt(10),
+                            100.0,
+                            p_pts,
+                            p_pts,
+                            p_pts,
+                            p_pts,
+                            p_pts,
+                            p_pts,
+                            p_pts,
+                            p_pts,
+                            p_pts,
+                            100.0,
+                            50.0,
+                            100.0
+                            );
+
+        for(int i = 0; i < NUMPOINTS; i++) {
+            x_pts[i] = i;
+            y_pts[i] = i;
+        }
+
+        cmv = CMV.initCMV(param, NUMPOINTS, x_pts, y_pts);
+
+        int i = 0;
+        for(Boolean lic : cmv) {
+            System.out.println("LIC" + i + '\t' + lic);
+            i++;
+        }
 
         int i;
         int j;
