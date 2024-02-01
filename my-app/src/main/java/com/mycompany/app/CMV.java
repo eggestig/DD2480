4a49d18dd3b09bd11df196bc3953aa1e86fb25ed
Anton Sederlin <anton.sederlin@gmail.com>
2024-02-01 12:14:18 +0100

    feat/#40 partial solution
    LIC 0-2 created and a couple of tests.
    
    Comit due to changes in main that i need to pull locally

--- my-app/src/main/java/com/mycompany/app/CMV.java
+++ my-app/src/main/java/com/mycompany/app/CMV.java
@@ -16,16 +16,97 @@ public class CMV {
         switch(LIC_num) {
             case(0):
                 return LIC0();
+            case(1):
+                return LIC1();
+            case(2):
+                return LIC2();
             default:
                 return false;
             }
     }
 
     private static Boolean LIC0() {
-        // TODO:
         // Return true if there exists at least one set of two consecutive data points
         // that are a distance greater than the length, LENGTH1, apart.
         // Else return false.
+
+        if (param.LENGTH1 < 0)
+            return false;
+
+        for(int i = 0; i < numPoints; i++) {
+            for(int j = i + 1; j < numPoints; j++) {
+                if(utils.dist(x_pts[i], y_pts[i], x_pts[j], y_pts[j]) > param.LENGTH1) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static Boolean LIC1(){
+        // Return true if there exists at least one set of three consecutive data points that cannot all be contained
+        // within or on a circle of radius RADIUS1. (0 ≤ RADIUS1)
+        // Else return false.
+
+        for(int i = 0; i < numPoints; i++) {
+            for(int j = i + 1; j < numPoints; j++) {
+                for(int k = j + 1; k < numPoints; k++) {
+
+                    int x1 = x_pts[i]; int y1 = y_pts[i];
+                    int x2 = x_pts[j]; int y2 = y_pts[j];
+                    int x3 = x_pts[k]; int y3 = y_pts[k];
+            
+                    Double a = utils.dist(x1, y1, x2, y2);
+                    Double b = utils.dist(x2, y2, x3, y3);
+                    Double c = utils.dist(x3, y3, x1, y1);
+            
+                    if(utils.circumRadius(a, b, c) > param.RADIUS1) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static Boolean LIC2() {
+        // Return true if there exists at least one set of three consecutive data points
+        // which form an angle such that: angle < (PI − EPSILON) or angle > (PI + EPSILON).
+        // Return false if either the first point or the last point (or both) coincides with the vertex.
+        // (0 ≤ EPSILON < PI)
+
+        // The second of the three consecutive points is always the vertex of the angle.
+        // If either the first point or the last point (or both) coincides with the vertex,
+        // the angle is undefined and the LIC is not satisfied by those three points.
+
+        for(int i = 0; i < numPoints; i++) {
+            for(int j = i + 1; j < numPoints; j++) {
+                for(int k = j + 1; k < numPoints; k++) {
+                    
+                    int x1 = x_pts[i];
+                    int y1 = y_pts[i];
+                    int x2 = x_pts[j]; // Vertex
+                    int y2 = y_pts[j]; // Vertex
+                    int x3 = x_pts[k];
+                    int y3 = y_pts[k];
+            
+                    Double n1 = utils.euclideanNorm(x1, y1, x2, y2);
+                    Double n2 = utils.euclideanNorm(x2, y2, x3, y3);
+            
+                    // Calculate the cosine of the angle between the vectors
+                    Double cosineAngle = utils.dotProduct(x1, y1, x2, y2, x3, y3) / (n1 * n2);
+            
+                    // Calculate the angle in radians
+                    Double angle = Math.acos(cosineAngle);
+            
+                    if(angle < (Math.PI - param.EPSILON) || angle > (Math.PI + param.EPSILON)) {
+                        return true;
+                    }
+                }
+            }
+        }
 
         return false;
     }
--- my-app/src/test/java/com/mycompany/app/CMVTest.java
+++ my-app/src/test/java/com/mycompany/app/CMVTest.java
@@ -1,6 +1,5 @@
 package com.mycompany.app;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -56,4 +55,121 @@ public class CMVTest
             assertFalse("LIC" + licNum, cmv[licNum]);
         }
     }
+
+     /**
+     * LIC 0 is given two consecutive points with dist > (0 = length) apart,
+     * and should evaluate to true.
+     */
+    @Test
+    public void valid_input_LIC_0_two_points_greater_dist()
+    {
+        double length1 = 0;
+        double[] x_pts = new double[]{0, 1};
+        double[] y_pts = new double[]{0, 2};
+
+        param = param.deepCopy();
+        param.setNUMPOINTS(2);
+        param.setXpoints(x_pts);
+        param.setYpoints(y_pts);
+        param.setLenght1(length1);
+        Boolean[] cmv = CMV.initCMV(param);
+
+        assertTrue(cmv[0]);
+
+    }
+
+    /**
+     * Test initializing CMV with all false LIC
+     */
+    @Test
+    public void valid_Input_LIC_0_two_points_smaller_dist()
+    {
+        double length1 = 10;
+        double[] x_pts = new double[]{0, 0};
+        double[] y_pts = new double[]{0, 0};
+
+        param = param.deepCopy();
+        param.setNUMPOINTS(2);
+        param.setXpoints(x_pts);
+        param.setYpoints(y_pts);
+        param.setLenght1(length1);
+        Boolean[] cmv = CMV.initCMV(param);
+
+        assertFalse(cmv[0]);
+    }
+
+    /**
+     * Test initializing CMV with all false LIC
+     */
+    @Test
+    public void invalid_valid_input_LIC_0_condition()
+    {
+
+    }
+
+    /**
+     * LIC 1 is provided with 3 cons 
+     */
+    @Test
+    public void valid_Input_LIC_1_points_in_circle()
+    {
+        double[] x_pts = new double[]{6, 2, 4};
+        double[] y_pts = new double[]{6, 2, 4};
+        double radius1 = 10;
+
+        param = param.deepCopy();
+        param.setNUMPOINTS(3);
+        param.setXpoints(x_pts);
+        param.setYpoints(y_pts);
+        param.setLenght1(radius1);
+        Boolean[] cmv = CMV.initCMV(param);
+
+        assertTrue(cmv[1]);
+    }
+
+    /**
+     * Test initializing CMV with all false LIC
+     */
+    @Test
+    public void valid_input_LIC_1_condition2()
+    {
+
+    }
+
+    /**
+     * Test initializing CMV with all false LIC
+     */
+    @Test
+    public void invalid_valid_Input_LIC_1_condition()
+    {
+
+    }
+
+    /**
+     * Test initializing CMV with all false LIC
+     */
+    @Test
+    public void valid_valid_input_LIC_2_condition1()
+    {
+
+    }
+
+    /**
+     * Test initializing CMV with all false LIC
+     */
+    @Test
+    public void valid_valid_Input_LIC_2_condition2()
+    {
+
+    }
+
+    /**
+     * Test initializing CMV with all false LIC
+     */
+    @Test
+    public void invalid_valid_Input_LIC_2_condition1()
+    {
+
+    }
+
 }
