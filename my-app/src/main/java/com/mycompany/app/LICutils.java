f9182ff6606b28c368acd483e35c32c2b4bebbfa
KelvinWat <kelvinwat1204@gmail.com>
2024-02-01 23:05:22 +0800

    feature/#44 Test case for LIC 12 partial compete
    a true test in CMVtest.java

--- my-app/src/main/java/com/mycompany/app/LICutils.java
+++ my-app/src/main/java/com/mycompany/app/LICutils.java
@@ -2,7 +2,7 @@ package com.mycompany.app;
 
 public class LICutils {
 
-    public static Double dist(int x1, int y1, int x2, int y2) {
+    public static Double dist(double x1, double y1, double x2, double y2) {
         return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
     }
     
--- my-app/src/test/java/com/mycompany/app/CMVTest.java
+++ my-app/src/test/java/com/mycompany/app/CMVTest.java
@@ -1,6 +1,5 @@
 package com.mycompany.app;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -67,4 +66,76 @@ public class CMVTest
             assertFalse("LIC" + licNum, cmv[licNum]);
         }
     }
+
+        //Test LIC 6 throws invalidN_PTSError for invalid N_PTS
+    //Invalid N_PTS if N_PTS < 3
+    @Test
+    public void valid_input() {
+        //return ture because dist between x,y[5] and x,y[9] < 17
+        double[] x_pts = new double[]{
+            37.84,
+            12.56,
+            65.23,
+            88.11,
+            42.79,
+            9.33,
+            51.45,
+            76.02,
+            23.67,
+            94.88
+        };
+        double[] y_pts = new double[]{
+            5.72,
+            68.94,
+            33.18,
+            79.57,
+            17.41,
+            54.26,
+            91.09,
+            36.75,
+            62.88,
+            8.05};
+        
+        int k_pts = 3;
+        double length2 = 17;
+
+        Parameters param = new Parameters(
+            10,
+            x_pts,
+            y_pts,
+            1.0,
+            2.0,
+            3.0,
+            4.0,
+            5,
+            6,
+            7.0,
+            8,
+            k_pts,
+            10,
+            11,
+            12,
+            13,
+            14,
+            15,
+            16,
+            length2,
+            18.0,
+            19.0
+        );
+
+        boolean[] cmv;
+        cmv = CMV.initCMV(param, param.getNUMPOINTS(), param.getX_PTS(), param.getY_PTS());
+        for(int licNum = 0; licNum < cmv.length; licNum++)
+        //System.out.println(CMV.LIC12());
+        
+        assertTrue(CMV.LIC12());
+        
+
+        
+        //cmv = CMV.initCMV(param);
+
+        // assert test throws custom error
+    }
+   
 }
