93f9eff4bd4cec356924c47bc33f66d8575dd629
Henrik Åkesson <hakes@kth.se>
2024-02-01 12:14:28 +0100

    Feature/#42
    Create LIC 5 - 8 in CMV.java. Create test for LIC 5 - 8 for valid and invalid inputs.

--- my-app/src/main/java/com/mycompany/app/CMV.java
+++ my-app/src/main/java/com/mycompany/app/CMV.java
@@ -16,6 +16,14 @@ public class CMV {
         switch(LIC_num) {
             case(0):
                 return LIC0();
+            case(5):
+                return LIC5();
+            case(6):
+                return LIC6();
+            case(7):
+                return LIC7();
+            case(8):
+                return LIC8();
             default:
                 return false;
             }
@@ -29,6 +37,103 @@ public class CMV {
 
         return false;
     }
+
+    private static Boolean LIC5() {
+        // Return true if there exists at least one set of two consecutive data points,
+        // (X[i],Y[i]) and (X[j],Y[j]), such that X[j] - X[i] < 0
+        // (where i = j-1).
+        // Else return false.
+
+        for(int i = 0; i < numPoints; i++) {
+            for(int j = i + 1; j < numPoints; j++) {
+                if(x_pts[j] - x_pts[i] < 0) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static Boolean LIC6() {
+        // Return true if there exists at least one set of N_PTS consecutive data points
+        // such that at least one of the points lies a distance greater than DIST from
+        // the line joining the first and last of these N_PTS points.
+        // Else return false.
+
+        // If the first and last points of these N_PTS are identical,
+        // then the calculated distance to compare with DIST will be the distance from the
+        // coincident point to all other points of the N_PTS consecutive points.
+        // The condition is not met when NUMPOINTS < 3.
+        // (3 ≤ N_PTS ≤ NUMPOINTS), (0 ≤ DIST)
+
+        if(x_pts.length < 3 || y_pts.length < 3) {return false;}
+
+        for(int i = 0; i < numPoints - param.N_PTS; i++) {
+            for(int j = i + 1; j < param.N_PTS; j++) {
+                if(utils.distToLine(x_pts[j], y_pts[j], x_pts[i], y_pts[i], x_pts[i + param.N_PTS], y_pts[i + param.N_PTS]) > param.DIST) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static Boolean LIC7() {
+        // Return true if there exists at least one set of two data points separated by
+        // exactly K_PTS consecutive intervening points that are a distance greater than
+        // the length, LENGTH1, apart.
+        // Else return false.
+
+        // The condition is not met when NUMPOINTS < 3.
+        // 1 ≤ K_PTS ≤ (NUMPOINTS − 2)
+
+        if(x_pts.length < 3 || y_pts.length < 3) {return false;}
+        
+        for(int i = 0; i < numPoints - param.K_PTS - 1; i++) {
+            if(utils.dist(x_pts[i], y_pts[i], x_pts[i + param.K_PTS + 1], y_pts[i + param.K_PTS + 1]) > param.LENGTH1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static Boolean LIC8() {
+        // Return true if there exists at least one set of three data points
+        // separated by exactly A_PTS and B_PTS consecutive intervening points,
+        // respectively, that cannot be contained within or on a circle of radius RADIUS1.
+        // Else return false.
+
+        // The condition is not met when NUMPOINTS < 5.
+        // 1 ≤ A_PTS, 1 ≤ B_PTS
+        // A_PTS + B_PTS ≤ (NUMPOINTS−3)
+
+        if(x_pts.length < 5 || y_pts.length < 5) {return false;}
+
+        for(int i = 0; i < numPoints - (param.A_PTS + param.B_PTS); i++) {
+            for(int j = i + param.A_PTS; j < numPoints - param.B_PTS; j++) {
+                for(int k = j + param.B_PTS; k < numPoints; k++) {
+
+                    int x1 = x_pts[i]; int y1 = y_pts[i];
+                    int x2 = x_pts[j]; int y2 = y_pts[j];
+                    int x3 = x_pts[k]; int y3 = y_pts[k];
+            
+                    Double a = utils.dist(x1, y1, x2, y2);
+                    Double b = utils.dist(x2, y2, x3, y3);
+                    Double c = utils.dist(x3, y3, x1, y1);
+            
+                    if(utils.circumRadius(a, b, c) > param.RADIUS1) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
 
     public static Boolean[] initCMV(Parameters PARAM, int NUMPOINTS, int[] xpts, int[] ypts) {
 
--- my-app/src/test/java/com/mycompany/app/CMVTest.java
+++ my-app/src/test/java/com/mycompany/app/CMVTest.java
@@ -18,8 +18,7 @@ public class CMVTest
      * Test initializing CMV with all false LIC
      */
     @Test
-    public void testCMV()
-    {
+    public void testCMV() {
         int NUMPOINTS = 10;
         int[] x_pts = new int[NUMPOINTS];
         int[] y_pts = new int[NUMPOINTS];
@@ -51,9 +50,141 @@ public class CMVTest
             y_pts[i] = i;
         }
         Boolean[] cmv;
-        cmv = CMV.initCMV(param, NUMPOINTS, x_pts, y_pts);
+        cmv = CMV.initCMV(param);
         for(int licNum = 0; licNum < cmv.length; licNum++) {
             assertFalse("LIC" + licNum, cmv[licNum]);
         }
+    }
+
+    // Test LIC 5 returns false if not two consecutive points exist with decreasing x
+    @Test
+    public void valid_input_LIC_5_no_two_consecutive_points_with_decreasing_x_returns_false() {
+        
+        double[] x_pts = new double[]{0.0, 1.0};
+        double[] y_pts = new double[]{0.0, 1.0};
+        
+        param = param.deepCopy();
+        param.setNUMPOINTS(2);
+        param.setXpoints(x_pts);
+        param.setYpoints(y_pts);
+
+        cmv = CMV.initCMV(param);
+
+        assertFalse("LIC 5", cmv[5]);
+
+    }
+
+    // Test LIC 5 returns true if two consecutive points exist with decreasing x
+    @Test
+    public void valid_input_LIC_5_two_consecutive_points_with_decreasing_x_returns_true() {
+        
+        double[] x_pts = new double[]{1.0, 0.0};
+        double[] y_pts = new double[]{0.0, 1.0};
+        
+        param = param.deepCopy();
+        param.setNUMPOINTS(2);
+        param.setXpoints(x_pts);
+        param.setYpoints(y_pts);
+
+        Boolean[] cmv;
+        cmv = CMV.initCMV(param);
+
+        assertTrue("LIC 5", cmv[5]);
+
+    }
+
+    // Test LIC 6 return true if there exists at least one set of N_PTS consecutive data points
+    // such that at least one of the points lies a distance greater than DIST from
+    // the line joining the first and last of these N_PTS points.
+    // (3 ≤ N_PTS ≤ NUMPOINTS), (0 ≤ DIST)
+    @Test
+    public void valid_input_LIC_6_n_points_exist_where_one_point_lies_valid_distance_from_line_joining_first_and_last_n_points_returns_true() {
+        
+        double[] x_pts = new double[]{0.0, 100.0, 1.0};
+        double[] y_pts = new double[]{0.0, 100.0, 1.0};
+        int n_pts = 3;
+        double dist = 1;
+        
+        param = param.deepCopy();
+        param.setNUMPOINTS(2);
+        param.setXpoints(x_pts);
+        param.setYpoints(y_pts);
+        param.setN_PTS(n_pts);
+        param.setDIST(dist);
+
+        Boolean[] cmv;
+        cmv = CMV.initCMV(param);
+
+        assertTrue("LIC 5", cmv[5]);
+
+    }
+
+    // Test LIC 6 return false if no set of N_PTS consecutive data points exist
+    // such that at least one of the points lies a distance greater than DIST from
+    // the line joining the first and last of these N_PTS points.
+    @Test
+    public void valid_input_LIC_6_n_points_no_one_point_lies_valid_distance_from_line_joining_first_and_last_n_points_returns_false() {
+        
+        double[] x_pts = new double[]{0.0, 1.1, 1.0};
+        double[] y_pts = new double[]{0.0, 1.1, 1.0};
+        int n_pts = 3;
+        double dist = 100;
+        
+        param = param.deepCopy();
+        param.setNUMPOINTS(2);
+        param.setXpoints(x_pts);
+        param.setYpoints(y_pts);
+        param.setN_PTS(n_pts);
+        param.setDIST(dist);
+
+        Boolean[] cmv;
+        cmv = CMV.initCMV(param);
+
+        assertFalse("LIC 5", cmv[5]);
+
+    }
+
+    //Test LIC 6 throws invalidN_PTSError for invalid N_PTS
+    //Invalid N_PTS if N_PTS < 3
+    @Test
+    public void invalid_input_LIC_6_N_PTS_less_than_3_throws_invalid_N_PTS_error() {
+        double[] x_pts = new double[]{0.0, 100.0, 1.0};
+        double[] y_pts = new double[]{0.0, 100.0, 1.0};
+        int n_pts = 0;
+        double dist = 1;
+        
+        param = param.deepCopy();
+        param.setNUMPOINTS(2);
+        param.setXpoints(x_pts);
+        param.setYpoints(y_pts);
+        param.setN_PTS(n_pts);
+        param.setDIST(dist);
+
+        Boolean[] cmv;
+        cmv = CMV.initCMV(param);
+
+        // assert test throws custom error
+    }
+
+    //Test LIC 6 returns false for invalid N_PTS
+    //Invalid N_PTS if N_PTS > NUMPOINTS
+    @Test
+    public void valid_input_LIC_6_N_PTS_greater_than_NUMPOINTS_returns_false() {
+        double[] x_pts = new double[]{0.0, 100.0, 1.0};
+        double[] y_pts = new double[]{0.0, 100.0, 1.0};
+        int n_pts = 4;
+        double dist = 1;
+        
+        param = param.deepCopy();
+        param.setNUMPOINTS(2);
+        param.setXpoints(x_pts);
+        param.setYpoints(y_pts);
+        param.setN_PTS(n_pts);
+        param.setDIST(dist);
+
+        Boolean[] cmv;
+        cmv = CMV.initCMV(param);
+
+        assertFalse("LIC 5", cmv[5]);
     }
 }
