7dd2a2ea0deb5f22bf2c1416d95aa2e0563b40ee
KelvinWat <kelvinwat1204@gmail.com>
2024-02-02 17:30:23 +0800

    fix/#94 modify CMVTest
    delete the TestCMV function, it was a function to test initCMV() before and is skipped. The cmv is test in DecideTest now

--- my-app/src/test/java/com/mycompany/app/CMVTest.java
+++ my-app/src/test/java/com/mycompany/app/CMVTest.java
@@ -16,61 +16,6 @@ import java.util.Arrays;
 public class CMVTest 
 {
     /**
-     * Test initializing CMV with all false LIC
-     */
-    @Test
-    public void testCMV()
-    {
-        Parameters param;
-        int numpoints;
-        double[] x_pts;
-        double[] y_pts;
-
-
-        Random rand = new Random();
-
-        numpoints = rand.nextInt(100 - 2) + 2;
-        x_pts = new double[numpoints];
-        y_pts = new double[numpoints];
-
-        Arrays.fill(x_pts, 0);
-        Arrays.fill(y_pts, 1);
-
-        param = new Parameters(
-            numpoints,
-            x_pts,
-            y_pts,
-            1.0,
-            2.0,
-            3.0,
-            4.0,
-            5,
-            6,
-            7.0,
-            8,
-            9,
-            10,
-            11,
-            12,
-            13,
-            14,
-            15,
-            16,
-            17.0,
-            18.0,
-            19.0
-        );
-
-        boolean[] cmv;
-        cmv = CMV.compute(param);
-        for(int licNum = 0; licNum < cmv.length; licNum++) {
-            //assertFalse("LIC" + licNum, cmv[licNum]);
-        }
-        assertTrue("Temp skip the test,", true);
-    }
-
-
-    /**
      * 
      * LIC 0 TESTS
      * 
